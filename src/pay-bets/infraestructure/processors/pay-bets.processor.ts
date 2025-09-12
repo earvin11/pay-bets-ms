9521ad7296b2e112363acb1f34363f756248b6dd
Earvin Gomez <earvinsantiago.11@gmail.com>
2025-09-12 11:13:48 -0400

    paybets processor

--- src/pay-bets/infraestructure/processors/pay-bets.processor.ts
+++ src/pay-bets/infraestructure/processors/pay-bets.processor.ts
@@ -1,15 +1,24 @@
 import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { LoggerPort } from 'src/logging/domain/logger.port';
+import { PayBetsUseCase } from 'src/pay-bets/application/pay-bets.use-case';
 import { QueueName } from 'src/shared/enums/queue-names.enum';
 
 @Processor(QueueName.PAY_BETS)
 export class PayBetsProcessor extends WorkerHost {
-  constructor(private readonly loggerPort: LoggerPort) {
+  constructor(
+    private readonly loggerPort: LoggerPort,
+    private readonly payBetsUseCase: PayBetsUseCase,
+  ) {
     super();
   }
 
-  process(job: Job, token?: string): Promise<any> {
-    throw new Error('Method not implemented.');
+  async process(job: Job, token?: string): Promise<any> {
+    try {
+      return await this.payBetsUseCase.run(job.data);
+    } catch (error) {
+      this.loggerPort.error(`[ERROR] process: ${QueueName.PAY_BETS}`, error);
+      throw error;
+    }
   }
 }
